acf3a407e9c26d888962f61f12e40b719dfcc21c
FimmeI <finnegankaiser@gmail.com>
2024-03-24 01:48:10 +0100

    Commit

--- js/script.js
+++ js/script.js
@@ -6,23 +6,6 @@ const faqs = document.querySelectorAll('.faq');
 
 var popupOpen = false;
 
-navItems.forEach(navItem => {
-
-  navItem.addEventListener('click', () => {
-
-    if (!popupOpen) {
-      if(navItem.id === '') {
-      navItems.forEach(navItem => {
-          navItem.classList.remove('active');
-      });
-
-        navItem.classList.add('active');
-    }
-    }
-  });
-
-});
-
 /*document.getElementById('content').addEventListener('click', () => {
     sidebar.classList.remove('open');
 });
@@ -31,16 +14,31 @@ document.getElementById('side').addEventListener('click', () => {
   sidebar.classList.add('open');
 });*/
 
-document.getElementById('side').addEventListener('mouseenter', () => {
-  if (!popupOpen)
-    sidebar.classList.add('open');
-});
 
-document.getElementById('side').addEventListener('mouseleave', () => {
-  if (!popupOpen)
-    sidebar.classList.remove('open');
-});
+/*=============== SHOW SIDEBAR ===============*/
+const showSidebar = (toggleId, sidebarId, mainId) =>{
+   const toggle = document.getElementById(toggleId),
+   sidebar = document.getElementById(sidebarId),
+   main = document.getElementById(mainId)
 
+   if(toggle && sidebar){
+       toggle.addEventListener('click', ()=>{
+           /* Show sidebar */
+           sidebar.classList.toggle('show-sidebar')
+       })
+   }
+}
+showSidebar('header-toggle','sidebar', 'main')
+
+/*=============== LINK ACTIVE ===============*/
+const sidebarLink = document.querySelectorAll('.sidebar__link')
+
+function linkColor(){
+    sidebarLink.forEach(l => l.classList.remove('active-link'))
+    this.classList.add('active-link')
+}
+
+sidebarLink.forEach(l => l.addEventListener('click', linkColor))
 
 faqs.forEach(faq => {
   const title = faq.querySelector('.faq-title');
@@ -104,39 +102,3 @@ faqs.forEach(faq => {
     }
   });*/
 });
-
-
-function getDateTime() {
-  var now     = new Date(); 
-  var year    = now.getFullYear();
-  var month   = now.getMonth()+1; 
-  var day     = now.getDate();
-  var hour    = now.getHours();
-  var minute  = now.getMinutes();
-  var second  = now.getSeconds(); 
-  if(month.toString().length == 1) {
-       month = '0'+month;
-  }
-  if(day.toString().length == 1) {
-       day = '0'+day;
-  }   
-  if(hour.toString().length == 1) {
-       hour = '0'+hour;
-  }
-  if(minute.toString().length == 1) {
-       minute = '0'+minute;
-  }
-  if(second.toString().length == 1) {
-       second = '0'+second;
-  }   
-  var dateTime = day+'.'+month+'.'+year+' '+hour+':'+minute+':'+second;   
-   return dateTime;
-}
-
-const progressBar = document.getElementById('progress-bar');
-const progressText = document.getElementById('progress-text');
-
-function updateProgressBar(percentage) {
-  progressBar.style.width = percentage + '%';
-  progressText.innerText = `Upload image: ${percentage}%`;
-}
